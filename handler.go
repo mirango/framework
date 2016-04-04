2c3dca2579ca51988383fc1d468529d800d69938
wlMalk <wm.wexem@gmail.com>
2016-04-04 06:47:06 -0500

    Defined framework Handler Interface

---
+++ handler.go
@@ -0,0 +1,11 @@
+package framework
+
+type Handler interface {
+	ServeHTTP(Context) interface{}
+}
+
+type HandlerFunc func(Context) interface{}
+
+func (f HandlerFunc) ServeHTTP(c Context) interface{} {
+	return f(c)
+}
