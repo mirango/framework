5c048f05ac58dfdc462ec77a7a5b4cf782c7ddea
wlMalk <wm.wexem@gmail.com>
2016-04-04 06:47:31 -0500

    Defining framework logging interfaces

---
+++ logger.go
@@ -0,0 +1,30 @@
+package framework
+
+import (
+	"io"
+)
+
+type Entry interface {
+	LogWriter
+}
+
+type EntryFormatter interface {
+	Format(Entry) string
+}
+
+type LogWriter interface {
+	Print(string, ...interface{})
+	Info(string, ...interface{})
+	Debug(string, ...interface{})
+	Warning(string, ...interface{})
+	Fatal(string, ...interface{})
+	With(...interface{}) Entry
+}
+
+type Logger interface {
+	LogWriter
+	Formatter(EntryFormatter)
+	DefaultWith(...interface{}) LogWriter
+	Writers(...io.Writer)
+	Logger(Context) Logger
+}
